14102835ad36b3a16ae9212c7e381c1b19190b24
Raphael Gregorius Hakim <77909888+Regieh@users.noreply.github.com>
2025-04-16 08:45:07 +0700

    Initialization

---
+++ belajar-git/belajar-git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  belajar-git
+//
+//  Created by Raphael Gregorius on 16/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ belajar-git/belajar-git/belajar_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  belajar_gitApp.swift
+//  belajar-git
+//
+//  Created by Raphael Gregorius on 16/04/25.
+//
+
+import SwiftUI
+
+@main
+struct belajar_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
