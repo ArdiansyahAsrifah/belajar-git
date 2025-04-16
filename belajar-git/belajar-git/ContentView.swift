95eaef4a06d8899fea8c9ce8657a8c7afeeacfbb
Raphael Gregorius Hakim <77909888+Regieh@users.noreply.github.com>
2025-04-16 08:55:10 +0700

    Update ContentView.swift

--- belajar-git/belajar-git/ContentView.swift
+++ belajar-git/belajar-git/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Halo Dunia")
+            Text("Halo Check")
         }
         .padding()
     }
